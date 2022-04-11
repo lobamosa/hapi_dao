0755fe3ef5da99f1e00798655477fcbf5d317103
namos <lobamo@salocina.com>
2022-04-12 00:17:26 +0200

    testing to get address

--- ethereum/test/Staking.test.js
+++ ethereum/test/Staking.test.js
@@ -18,17 +18,30 @@ describe('staking', () => {
         expect(await stakingContract.balanceOf(address1.address)).to.equal(100)
         //xpect(await stakingContract.isStakHolder(address1).to.equal(true))   
     })
-
-    it("should verify if address is a stakholder", async () => {
-        const [owner, address1] = await ethers.getSigners();
+    it("should return the address of the stakholder", async () => {
+        const [owner, address1, address2] = await ethers.getSigners();
         const supply = 10000;
         const name = "hapi";
         const symbol = "hapi";
         const staking = await ethers.getContractFactory("StakingToken")
         const stakingContract = await staking.deploy(owner.address, supply, name, symbol);
-        stakingContract.addStackHolder(address1.address, 1);
+        await stakingContract.addStackHolder(address1.address, 100);
+        const stakHolder = await stakingContract.getStakHolder(address1.address);
+        const stackHolderAddress = await stakingContract.getStakHolderAddress(address1.address);
+        expect(await stakingContract.getStakHolderAddress(stackHolderAddress)).to.equal(address1.address)
+
+    })
+    it("should verify if address is a stakholder", async () => {
+        const [owner, address1, address2] = await ethers.getSigners();
+        const supply = 10000;
+        const name = "hapi";
+        const symbol = "hapi";
+        const staking = await ethers.getContractFactory("StakingToken")
+        const stakingContract = await staking.deploy(owner.address, supply, name, symbol);
+        stakingContract.addStackHolder(address1.address, 100);
         const stackHolder = stakingContract.getStakHolder(address1.address);
-        console.log(stackHolder);
-        expect(await stakingContract.isStakHolder(address1.address, stackHolder));
+        stakingContract.addStackHolder(address2.address, 100)
+        const stackHolder2 = stakingContract.getStakHolder(address2.address)
+        expect(await stakingContract.isStakHolder(address1.address, stackHolder)).to.equal(true)
     })
 })
